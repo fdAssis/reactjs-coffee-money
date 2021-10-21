80b06867d25bbb6a1d0d402a466b942a625afc45
fdAssis <franciscodeassisc.santos@gmail.com>
2021-10-20 22:48:53 -0300

    add global stylest

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GobalStyles } from "./styles/globla";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello Word</h1>
+      <GobalStyles />
     </div>
   );
 }
