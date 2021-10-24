4dc8bbbf90b22285fbcae5426a0c777c1945fb6a
fdAssis <franciscodeassisc.santos@gmail.com>
2021-10-23 23:05:42 -0300

    add Modal Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Header";
-import { GlobalStyles } from "./styles/globla";
+import { useState } from 'react';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import Modal from 'react-modal';
+import { GlobalStyles } from './styles/global';
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [
+    isNewTransactionModalOpen, 
+    setIsNewTransactionModalOpen,
+  ] = useState(false); 
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
    <>
-    <Header />
+    <Header
+      onOpenNewTransitionModal={handleOpenNewTransactionModal}
+    />
+
     <Dashboard />
+
+    <Modal
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+    >
+      <h2>Cadastrar Transação</h2>
+    </Modal>
+
     <GlobalStyles />
    </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from "./styles";
+import Modal from 'react-modal';
 
-export function Header(){
+interface HeaderProps{
+  onOpenNewTransitionModal: () => void;
+}
+
+export function Header({onOpenNewTransitionModal} : HeaderProps){
+
+
   return(
     <Container>
       <Content>
         <img src={logoImg} alt="coffee-money"/>
-        <button type="button">Nova Transação</button>
+        <button
+          type="button"
+          onClick={onOpenNewTransitionModal}
+        >Nova Transação
+        </button>
       </Content>
     </Container>
   )
