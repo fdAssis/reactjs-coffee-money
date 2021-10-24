d249f3e2b270f7747a88b2500a1bfcbe8f35d5bf
fdAssis <franciscodeassisc.santos@gmail.com>
2021-10-24 17:39:28 -0300

    add select color button

--- src/components/NewTransitionModal/index.tsx
+++ src/components/NewTransitionModal/index.tsx
@@ -1,13 +1,21 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransitionTypeContainer, ButtonBox } from './styles';
+
 import ImgClose from '../../assets/close.svg';
+import ImgIncome from '../../assets/income.svg';
+import ImgOutcome from '../../assets/outcome.svg';
+import { useState } from 'react';
+
 
 interface NewTransitionModalProps{
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransitionModal({isOpen, onRequestClose}:NewTransitionModalProps){
+export function NewTransitionModal({isOpen,  onRequestClose}:NewTransitionModalProps){
+
+  const [type, setType] = useState('deposit');
+
   return (
      <Modal
       isOpen={isOpen}
@@ -27,6 +35,32 @@ export function NewTransitionModal({isOpen, onRequestClose}:NewTransitionModalPr
         <h2>Cadastrar Transação</h2>
         <input placeholder='Titulo' />
         <input type="number" placeholder='Valor' />
+        
+        <TransitionTypeContainer>
+          <ButtonBox
+            type="button"
+            onClick={() => {setType('deposit')}}
+            isSelected={type === 'deposit'}
+            selectColor='green'
+          >
+            <img src={ImgIncome} alt="Entrada" />
+            <span>Entrada</span>
+            
+          </ButtonBox>
+
+          <ButtonBox
+            type="button"
+            onClick={() => {setType('withdraw')}}
+            isSelected={type === 'withdraw'}
+            selectColor='red'
+          >
+            <img src={ImgOutcome} alt="Saida" />
+            <span>Saida</span>
+            
+          </ButtonBox>
+
+        </TransitionTypeContainer>
+
         <input placeholder='Categoria' />
         <input type="date" placeholder='Data' />
         <button type="submit">Cadastrar</button>
--- src/components/NewTransitionModal/styles.ts
+++ src/components/NewTransitionModal/styles.ts
@@ -46,6 +46,56 @@ export const Container = styled.form`
       filter: brightness(0.9);
     }
   }
-
 `;
 
+export const TransitionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`
+
+interface ButtonBoxProps{
+  isSelected: boolean;
+  selectColor: 'green' | 'red';
+}
+
+const colors = {
+  green: 'rgb(51, 204, 149, 0.2)',
+  red: 'rgb(229, 46, 77, 0.2)',
+}
+
+
+export const ButtonBox = styled.button<ButtonBoxProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 4rem;
+  background: ${(props) => props.isSelected 
+    ? colors[props.selectColor]
+    : '#E7E9EE'
+  };
+  
+  border: 1px solid #D7D7D7;
+  border-radius: 0.25rem;
+
+  transition: border 0.2s;
+
+  img{
+    width: 24px;
+    height: 24px;
+  }
+
+  &:hover{
+    border: 2px solid #C9D9D9;
+  }
+
+  span{
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1.2rem;
+    color: var(--text-title);
+  }
+`;
